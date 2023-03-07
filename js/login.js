cab99c0c7c09649dd5a5427e65b3373e02030f69
ahmad16syed <86577972+ahmad16syed@users.noreply.github.com>
2023-03-07 15:07:36 +0530

    added all files

---
+++ js/login.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.login-form').submit(function(e) {
+      e.preventDefault();
+      var email = $('.email').val();
+      var password = $('.assword').val();
+      $.ajax({
+        type: 'POST',
+        url: 'php/login.php',
+        data: {email: email, password: password},
+        success: function(data) {
+          if (data == "success") {
+            window.location.href = "profile.php";
+          } else {
+            alert(data);
+          }
+        }
+      });
+    });
+  });
